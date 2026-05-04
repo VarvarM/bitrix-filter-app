2cb6f173fe6296269e5f8b9c3e17e12ab3f0cf43
VarvarM <86892692+VarvarM@users.noreply.github.com>
2026-05-04 12:42:04 +0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,13 +1,21 @@
+import { readFileSync } from 'fs';
+import { join } from 'path';
+
 export default function handler(req, res) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', '*');
-    
+
     if (req.method === 'OPTIONS') {
         res.status(200).end();
         return;
     }
-    
-    // Редиректим на index.html
-    res.redirect(302, '/');
+
+    try {
+        const html = readFileSync(join(process.cwd(), 'index.html'), 'utf8');
+        res.setHeader('Content-Type', 'text/html');
+        res.status(200).send(html);
+    } catch(e) {
+        res.status(500).send('Error: ' + e.message);
+    }
 }
